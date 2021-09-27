d0dfaa71dd160ee39d6626e3e2f6fae60b1933ee
Pedro Martín del Campo González <pedro.martindelcampo.gonzalez@gmail>
2021-09-26 21:46:06 -0700

    Upload to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,21 @@
-export default {
-    base: '/daily-ui/'
-}
+const { defineConfig } = require("vite");
+const fs = require('fs');
+const { resolve } = require('path');
+
+const challenges = fs.readdirSync('./challenges')
+    .reduce((obj, current) => ({
+        ...obj,
+        [`challenge_${current.substring(0,3)}`]: resolve(__dirname, `challenges/${current}`)
+    }), { });
+
+module.exports = defineConfig({
+    base: '/daily-ui/',
+    build:{
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                ...challenges
+            }
+        }
+    }
+});
